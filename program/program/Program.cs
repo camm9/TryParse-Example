66ee628a065194eb57764165bf727a432384fad5
camm9 <105569979+camm9@users.noreply.github.com>
2022-07-02 10:01:15 -0700

    Update Program.cs
    adds do while loop

--- program/program/Program.cs
+++ program/program/Program.cs
@@ -10,40 +10,51 @@ namespace program
 
         public static void enterName()
         {
-            Console.WriteLine("Enter your name: ");
-            string name = Console.ReadLine();
-       
-
-            if (name.ToLower() == "jaspreet kaur sahiwal")
-            {
-                Console.WriteLine(name +" - Instructor");
-            }
-            else
-            {
-                Console.WriteLine(name + " - Student");
-            }
-
-            if (name.ToLower() != "jaspreet kaur sahiwal")
+            string name;
+            do
             {
 
-            Console.WriteLine("Enter your age: ");
-            string inputAge = Console.ReadLine();
-            bool isValidAge = int.TryParse(inputAge, out int age);
-                if (!isValidAge)
+                Console.WriteLine("Enter your name: ");
+                name = Console.ReadLine();
+                if (name.ToLower().Equals("exit"))
                 {
                     return;
                 }
 
-                if (age < 21)
+                if (name.ToLower() == "jaspreet kaur sahiwal")
                 {
-                    int years = 21 - age;
-                    Console.WriteLine("You must wait " + years + " years to enroll in this course");
+                    Console.WriteLine(name + " - Instructor");
                 }
+                
                 else
                 {
-                    Console.WriteLine("You are enrolled.");
+                    Console.WriteLine(name + " - Student");
                 }
-            }
+
+                if (name.ToLower() != "jaspreet kaur sahiwal")
+                {
+
+                    Console.WriteLine("Enter your age: ");
+                    string inputAge = Console.ReadLine();
+                    bool isValidAge = int.TryParse(inputAge, out int age);
+                    if (!isValidAge)
+                    {
+                        return;
+                    }
+
+
+
+                    if (age < 21)
+                    {
+                        int years = 21 - age;
+                        Console.WriteLine("You must wait " + years + " years to enroll in this course");
+                    }
+                    else
+                    {
+                        Console.WriteLine("You are enrolled.");
+                    }
+                }
+            } while (name != "exit");
 
 
 
@@ -52,3 +63,4 @@ namespace program
 
     }
 }
+
