f14960b7809adcb0dbeee606915fc531e3f5caf7
camm9 <105569979+camm9@users.noreply.github.com>
2022-07-02 10:00:29 -0700

    Add files via upload

---
+++ program/program/Program.cs
@@ -0,0 +1,54 @@
+﻿
+namespace program
+{
+    class program
+    {
+        static void Main(string[]args)
+        {
+            enterName();
+        }
+
+        public static void enterName()
+        {
+            Console.WriteLine("Enter your name: ");
+            string name = Console.ReadLine();
+       
+
+            if (name.ToLower() == "jaspreet kaur sahiwal")
+            {
+                Console.WriteLine(name +" - Instructor");
+            }
+            else
+            {
+                Console.WriteLine(name + " - Student");
+            }
+
+            if (name.ToLower() != "jaspreet kaur sahiwal")
+            {
+
+            Console.WriteLine("Enter your age: ");
+            string inputAge = Console.ReadLine();
+            bool isValidAge = int.TryParse(inputAge, out int age);
+                if (!isValidAge)
+                {
+                    return;
+                }
+
+                if (age < 21)
+                {
+                    int years = 21 - age;
+                    Console.WriteLine("You must wait " + years + " years to enroll in this course");
+                }
+                else
+                {
+                    Console.WriteLine("You are enrolled.");
+                }
+            }
+
+
+
+
+        }
+
+    }
+}
